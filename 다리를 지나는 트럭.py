b0d3173534c9c81a5111b17aa31752818ed19070
rightstone <zjohn2z@naver.com>
2020-08-21 01:06:09 +0900

    commit3

--- 다리를 지나는 트럭.py
+++ 다리를 지나는 트럭.py
@@ -1,3 +1,21 @@
+'''
+input
+'''
 bridge_length = 2
 weight = 10
 truck_weight = [7, 4, 5, 6]
+
+bridge = [0]*bridge_length
+count = 0
+
+while bridge:
+    count += 1
+    bridge.pop(0)
+    if truck_weight:
+        next_truck = truck_weight.pop(0)
+        if sum(bridge)+next_truck <= weight:
+            bridge.append(next_truck)
+        else:
+            bridge.append(0)
+
+print(count)
