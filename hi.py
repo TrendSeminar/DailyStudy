8d92ed1834fa1a26cd3e00d062dde5459c5e1d0d
rightstone <zjohn2z@naver.com>
2020-08-22 13:06:10 +0900

    s

--- hi.py
+++ hi.py
@@ -11,3 +11,13 @@ numbers의 길이는 1 이상 100,000 이하입니다.
 numbers의 원소는 0 이상 1,000 이하입니다.
 정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
 '''
+numbers = [6, 10, 2]
+
+numbers = [str(x) for x in numbers]
+numbers.sort(key=lambda x: (x*4)[:4], reverse=True)
+
+answer = "".join(numbers)
+if len(numbers) == 1 and numbers[0] == '0':
+    answer = '0')
+
+print(answer)
